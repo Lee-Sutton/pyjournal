cc6754793e7f171d1dffe5ec5a24cd143096416a
Lee Sutton <leesutton1@gmail.com>
2018-12-07 19:42:50 -0800

    added get active issues function

--- pyjournal/jira_connection.py
+++ pyjournal/jira_connection.py
@@ -1,18 +1,15 @@
 from jira import JIRA
 
+DEFAULT_BOARD = 1
+
 
 class Jira:
-    pass
-#     def __init__(self, url, email, password):
-#
-#
-# def test_jira_connection(url, email, password):
-#     JIRA(url, auth=(email, password))
-#
-#
-# def get_active_issues(jira_connection):
-#     for sprint in jira_connection.sprints(board_id=1):
-#         if sprint.state == 'ACTIVE':
-#             return jira_connection.search_issues(f'sprint={sprint.id} AND assignee = currentUser()')
-#
-#
+    def __init__(self, url, email, password):
+        self._jira = JIRA(url, auth=(email, password))
+
+    def get_active_issues(self, board_id=DEFAULT_BOARD):
+        """Searches for active issues on the input board"""
+        print(self._jira)
+        for sprint in self._jira.sprints(board_id=board_id):
+            if sprint.state == 'ACTIVE':
+                return self._jira.search_issues(f'sprint={sprint.id} AND assignee = currentUser()')
