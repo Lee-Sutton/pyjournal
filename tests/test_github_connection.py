1cd065874be8c00ec0fc90d7455aa0d9a5820ddf
Lee Sutton <leesutton1@gmail.com>
2019-01-10 19:09:06 -0800

    refactored github pytest fixtures

--- tests/test_github_connection.py
+++ tests/test_github_connection.py
@@ -1,8 +1,25 @@
-from pyjournal.github_connection import GithubConnection
 from unittest.mock import patch
 
+import pytest
 
-@patch('pyjournal.github_connection.Github')
+from pyjournal.github_connection import GithubConnection
+
+
+@pytest.fixture
+def mock_github_api():
+    patcher = patch('pyjournal.github_connection.Github')
+    yield patcher.start()
+    patcher.stop()
+
+
+@pytest.fixture()
+def github_connection(mock_github_api, fake):
+    username = fake.name()
+    password = 'password'
+
+    yield GithubConnection(username, password)
+
+
 def test_github_connection(mock_github_api, fake):
     username = fake.name()
     password = 'password'
@@ -10,4 +27,5 @@ def test_github_connection(mock_github_api, fake):
     GithubConnection(username, password)
     mock_github_api.assert_called_with(username, password)
 
-
+def test_get_issues():
+    pass
