d18f056dd3675685e6b26764591d2f3818c303c9
Lee Sutton <leesutton1@gmail.com>
2019-01-11 09:49:03 -0800

    get prs awaiting review

--- pyjournal/github_connection.py
+++ pyjournal/github_connection.py
@@ -3,8 +3,12 @@ from github import Github
 
 class GithubConnection:
     """Connects to gihub api"""
+
     def __init__(self, username, password):
+        self._username = username
+        self._password = password
         self._github = Github(username, password)
 
-    def get_issues(self):
-        pass
+    def get_prs_awaiting_review(self):
+        return self._github.search_issues(
+            f'is:open is:pr review-requested:{self._username}')
--- tests/test_github_connection.py
+++ tests/test_github_connection.py
@@ -12,14 +12,6 @@ def mock_github_api():
     patcher.stop()
 
 
-@pytest.fixture()
-def github_connection(mock_github_api, fake):
-    username = fake.name()
-    password = 'password'
-
-    yield GithubConnection(username, password)
-
-
 def test_github_connection(mock_github_api, fake):
     username = fake.name()
     password = 'password'
@@ -27,5 +19,13 @@ def test_github_connection(mock_github_api, fake):
     GithubConnection(username, password)
     mock_github_api.assert_called_with(username, password)
 
-def test_get_issues():
-    pass
+
+def test_get_prs_awaiting_review(mock_github_api, fake):
+    """It should return a list of issues awaiting review"""
+    username = fake.name()
+    password = 'password'
+
+    github_connection = GithubConnection(username, password)
+    github_connection.get_prs_awaiting_review()
+    mock_github_api().search_issues.assert_called_with(
+        f'is:open is:pr review-requested:{username}')
